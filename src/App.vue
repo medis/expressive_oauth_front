21b17079bc2706653451dc7ed73563ac1df9d846
medis <aubuka@gmail.com>
2018-12-25 16:19:55 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" class="container flex content-center mx-auto w-full py-8 h-screen items-center">
+    <div class="bg-white shadow-md rounded rounded mx-auto">
+      <Login v-if="!user"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Login from './components/Login.vue'
+
+export default {
+  name: 'app',
+  data: function() {
+      return {
+          user: window.App && window.App.user ? window.App.user : false
+      }
+  },
+  components: {
+      Login
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="max-w-xs w-full px-8 pt-6 pb-8">
+    <form action="POST">
+      <label for="username" class="label">Username
+        <input id="username" class="input" type="text" name="username" placeholder="Username" />
+      </label>
+      <label for="password" class="label">Password
+        <input id="password" class="input" type="password" placeholder="Password" />
+      </label>
+
+      <div class="flex items-center justify-between">
+        <button class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Sign in</button>
+        <a href="#" class="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker">Register</a>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
